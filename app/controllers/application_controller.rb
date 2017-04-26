763db382a9aa5a9cfef54be3a55f17f5e5452bf0
Jankees van Woezik <mail@jankeesvw.com>
2017-04-26 09:43:46 +0200

    Make auth configurable
    Also remove the default user name and password and the URL to our
    deployment (which is irrelevant).

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  http_basic_authenticate_with name: "ultimaker", password: "ultimaker2011"
+  http_basic_authenticate_with name: ENV["BASIC_AUTH_USER_NAME"], password: ENV["BASIC_AUTH_PASSWORD"]
 
   rescue_from Google::Apis::AuthorizationError, Google::Apis::ClientError do
     redirect_to authorize_url
