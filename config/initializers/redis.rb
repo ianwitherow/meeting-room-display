a22c93c873e3091e1aac6935d4a6753b61ce7c6f
Ian Witherow <ian.witherow@sourceot.com>
2017-08-25 10:53:39 -0600

    Updates redis config

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,3 @@
-redis_uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/" )
-$redis = Redis.new(host: redis_uri.host, port: redis_uri.port, password: redis_uri.password)
+uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/" )
+
+$redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
